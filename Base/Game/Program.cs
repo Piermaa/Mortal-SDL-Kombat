fad2a5d67f6be9b3b2147f903085a5f37d07737f
Fabrizio Piermarocchi <fabrizio.pier.dv@gmail.com>
2023-03-17 13:31:26 -0300

    Maid Moduling 1

--- Base/Game/Program.cs
+++ Base/Game/Program.cs
@@ -10,7 +10,7 @@ namespace TestEngine
         public  Vector2 position=new Vector2(0,0);
         public  Vector2 velocity=Vector2.Zero;
         public Vector2 acceleration= Vector2.Zero;
-
+        public bool isGrounded=false;
         public float mass=65;
        // float movementSpeed=1;
 
@@ -37,11 +37,13 @@ namespace TestEngine
         public static float endTime;
         static float movementVelocity = 250f;
 
+        static float jumpForce=700;
+        static float xOffset = 0;
 
         //If main is static all functions must be static
         static void Main(string[] args)
         {
-            Game.Initialize("Moirtal Kombat XXX",WIDTH,HEIGHT,false);
+            Game.Initialize("Mortal Kombat XXX",WIDTH,HEIGHT,false);
             InitializePlayers();
             while (true)
             {
@@ -64,37 +66,55 @@ namespace TestEngine
             GetTime();
             InputMovement();
 
-            Game.Draw("bg.png", 0, 0, 1, 1, 0, 550, 0);
-            Game.Draw(player1.textureName, player1.position.x, player1.position.y, 0.5f, 0.5f, 0, 0);
-            Game.Draw(player2.textureName, player2.position.x, player2.position.y, 0.5f, 0.5f, 0, 0);
+            Game.Draw("bg.png", 0, 0, 1, 1, 0, 550+xOffset, 0);
+         
+            Game.Draw(player1.textureName, player1.position.x, player1.position.y, 0.5f, 0.5f, 0,0, 0);
+            Game.Draw(player2.textureName, player2.position.x, player2.position.y, 0.5f, 0.5f, 0,0, 0);
 
-
+      
 
             //Finally, we show it
             Game.Show();
         }
-
+        /// <summary>
+        /// Se calcula el movimiento de los jugadores
+        /// </summary>
         private static void InputMovement()
         {
-            float x = GetAxisRaw("Horizontal");
-            float y = GetAxisRaw("Vertical");
+            float x1 = GetAxisRaw("Horizontal");
+            float y1 = GetAxisRaw("Vertical");
 
-            if (y<0)
+            float x2 = GetAxisRaw("Horizontal2");
+            float y2 = GetAxisRaw("Vertical2");
+
+            if (y1<0 && player1.isGrounded)
             {
-                player1.velocity.y = -500;
+                player1.velocity += Vector2.Up *jumpForce;
+                player1.isGrounded=false;
+            }
+            if (y2<0 && player2.isGrounded)
+            {
+          
+                player2.velocity += Vector2.Up* jumpForce;
+                player2.isGrounded = false;
             }
 
-            player1.velocity = new Vector2(x*movementVelocity,player1.velocity.y);
-            
-       
+            RigidBody.Move(player1, x1,movementVelocity);
+            RigidBody.Move(player2, x2, movementVelocity);
 
-            RigidBody(player1);
-            RigidBody(player2);
-  
+            RigidBody.UpdateRigidBody(player1,GRAVITY,deltaTime);
+            RigidBody.UpdateRigidBody(player2,GRAVITY,deltaTime);
         }
 
+        /// <summary>
+        /// Se obtiene el valor bruto de un eje de movimiento dependiendo de la Input actual 
+        /// </summary>
+        /// <param name="axis">Nombre del eje, pej: Horizontal. Horizontal2 es para las ArrowKeys, input del player 2</param>
+        /// <returns>Un valor que puede ser -1, 0 o 1 dependiendo del Input del jugador</returns>
         static int GetAxisRaw(string axis)
         {
+
+            
             switch (axis)
             {
                 case "Horizontal":
@@ -118,12 +138,39 @@ namespace TestEngine
                         return 1; // para abajo positivo
                     }
                     break;
+
+
+
+                case "Horizontal2":
+                    if (Game.GetKey(Keys.LEFT))
+                    {
+                        return -1;
+                    }
+                    if (Game.GetKey(Keys.RIGHT))
+                    {
+                        return 1;
+                    }
+                    break;
+
+                case "Vertical2":
+                    if (Game.GetKey(Keys.UP))
+                    {
+                        return -1;
+                    }
+                    if (Game.GetKey(Keys.DOWN))
+                    {
+                        return 1; // para abajo positivo
+                    }
+                    break;
             }
             return 0;
         }
 
-    
+        
 
+        /// <summary>
+        /// Se obtiene el valor de deltaTime
+        /// </summary>
         static void GetTime()
         {
             var currentTime = (float)(DateTime.Now - startTime).TotalSeconds;
@@ -131,33 +178,6 @@ namespace TestEngine
             endTime = currentTime;
         }
 
-
-        static void RigidBody(Player b)
-        {
-            AddForce(b, new Vector2(0, GRAVITY*b.mass));
-
-
-            b.velocity.x += b.acceleration.x * deltaTime;
-            b.velocity.y += b.acceleration.y * deltaTime;
-
-            b.position.x += b.velocity.x * deltaTime;
-            b.position.y += b.velocity.y * deltaTime;
-
-            b.acceleration.x = 0;
-            b.acceleration.y = 0;
-
-            if (b.position.y>200)
-            {
-                b.position.y = 200;
-            }
-        }
-
-        //---------------------------------------agregar fuerza------------------------------------------
-        static void AddForce(Player b, Vector2 direction)
-        {
-            b.acceleration.x += direction.x / b.mass;
-            b.acceleration.y += direction.y / b.mass;
-
-        }
+       
     }
 }
