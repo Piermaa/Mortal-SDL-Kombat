8ea9f237164ff3341b667af1245614fe621c806f
Juan Fangano <juanyfanga@gmail.com>
2023-05-23 01:30:01 -0300

    Animations (Empezado) y  Factory (Empezado)

--- Base/Game/Enemies/EnemyCharacter.cs
+++ Base/Game/Enemies/EnemyCharacter.cs
@@ -10,9 +10,18 @@ namespace Game
     {
         private float shootCD=2;
         private float shootTimer;
+        //Animator explosionAnimator = new Animator();
+        //SpriteRenderer explosionSpriteRenderer = new SpriteRenderer();
+
+        private const string EXPLOSIONANIMATION = "Explosion";
 
         public EnemyCharacter(GameObject _gameObject, string textureName, float attackSpeed) : base(_gameObject)
         {
+            // Usé las mismas que el de los engines porque me quedé sin internet y no pude buscar otras xd
+            //explosionAnimator.CreateAnimation(EXPLOSIONANIMATION, "Textures/Animations/Engine/", 3, 0.1f);
+            //_gameObject.AddComponent(explosionAnimator);
+            //_gameObject.AddComponent(explosionSpriteRenderer);
+
             AddSprite(textureName);
             shootCD = attackSpeed;
         }
@@ -33,5 +42,11 @@ namespace Game
                 bulletGameObject.BulletReset(transform.position, 180, false);
             }
         }
+
+        //public override void Death()
+        //{
+        //    explosionAnimator.SetAnimation(EXPLOSIONANIMATION);
+        //    base.Death();
+        //}
     }
 }
---
+++ Base/Game/EnemyFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    //public static class EnemyFactory
+    //{
+    //    public enum TypeOfEnemy { Normal, Heavy, Boss}
+
+    //    public static IEnemyType CreateEnemy (TypeOfEnemy type)
+    //    {
+    //        switch (type)
+    //        {
+    //            case TypeOfEnemy.Normal:
+    //                return new EnemyCharacter();
+    //                break;
+
+    //            case TypeOfEnemy.Heavy:
+    //                return new EnemyCharacter();
+    //                break;
+
+    //            case TypeOfEnemy.Boss:
+    //                return new EnemyCharacter();
+    //                break;
+    //        }
+
+    //        return null;
+    //    }
+    //}
+}
